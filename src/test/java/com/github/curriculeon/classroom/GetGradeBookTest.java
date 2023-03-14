4481c9e8b4263b707062becccfe343777246d042
git-leon <xleonhunter@gmail.com>
2023-03-14 16:46:41 -0400

    updated tests

--- src/test/java/com/github/curriculeon/classroom/GetGradeBookTest.java
+++ src/test/java/com/github/curriculeon/classroom/GetGradeBookTest.java
@@ -3,29 +3,27 @@ package com.github.curriculeon.classroom;
 import com.github.curriculeon.Classroom;
 import com.github.curriculeon.Student;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
 
 public class GetGradeBookTest {
-    private Classroom classroom;
 
-    @Before
-    public void setUp() {
-        // Create a new classroom with some students
-        Student alice = new Student("Alice", "Smith", new Double[] { 80.0, 90.0, 95.0 });
-        Student bob = new Student("Bob", "Jones", new Double[] { 75.0, 85.0, 90.0 });
-        Student charlie = new Student("Charlie", "Brown", new Double[] { 85.0, 95.0, 100.0 });
-        classroom = new Classroom(new Student[]{alice, bob, charlie});
-    }
-
+    // given
     @Test
     public void testGetGradeBook() {
+        // Create a new classroom with some students
+        Student alice = new Student("Alice", "Smith", new Double[]{80.0, 90.0, 95.0});
+        Student bob = new Student("Bob", "Jones", new Double[]{75.0, 85.0, 90.0});
+        Student charlie = new Student("Charlie", "Brown", new Double[]{85.0, 95.0, 100.0});
+        Classroom classroom = new Classroom(new Student[]{alice, bob, charlie});
+
+        // when
         // Get the grade book and make sure it's not null
         Map<Student, Double> gradeBook = classroom.getGradeBook();
         Assert.assertNotNull(gradeBook);
 
+        // then
         // Check that the grade book contains the expected students and grades
         Assert.assertEquals(3, gradeBook.size());
         Assert.assertTrue(gradeBook.containsKey(new Student("Alice", "Smith", null)));
