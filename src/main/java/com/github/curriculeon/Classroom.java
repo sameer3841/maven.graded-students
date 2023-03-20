1bdde47688e48e9ba428c6dbf89ec67f5f5e116a
git-leon <xleonhunter@gmail.com>
2023-03-19 21:16:28 -0400

    gutted solution

--- src/main/java/com/github/curriculeon/Classroom.java
+++ src/main/java/com/github/curriculeon/Classroom.java
@@ -1,82 +1,40 @@
 package com.github.curriculeon;
 
-import java.util.*;
+
+import java.util.Map;
 
 public class Classroom {
-    private List<Student> students;
 
     public Classroom(Student[] students) {
-        this.students = new LinkedList<>(Arrays.asList(students));
     }
 
     public Classroom() {
-        this(new Student[]{});
     }
 
     public Student[] getStudents() {
-        return students.toArray(new Student[0]);
+        return null;
     }
+
 
     public Double getAverageExamScore() {
-        return students
-                .stream()
-                .mapToDouble(Student::getAverageExamScore)
-                .average()
-                .orElse(0);
+        return null;
     }
 
-    public Boolean addStudent(Student student) {
-        if (students.contains(student)) {
-            return false;
-        }
-        students.add(student);
-        return true;
+    public Boolean addStudent(Student students) {
+        return null;
     }
 
     public Boolean removeStudent(Student student) {
-        if (!students.contains(student)) {
-            return false;
-        }
-        students.remove(student);
-        return true;
+        return null;
     }
 
     public Student[] getStudentsByScore() {
-        return students
-                .stream()
-                .sorted()
-                .toArray(Student[]::new);
+        return null;
     }
 
-    public Map<Student, Character> getGradeBook() {
-        Map<Student, Character> gradeBook = new HashMap<>();
-        int numStudents = students.size();
-        if (numStudents == 0) {
-            return gradeBook;
-        }
-        // Sort students by average exam score
-        Student[] sortedStudents = getStudentsByScore();
-        // Determine grade cutoffs based on percentile ranks
-        // Assign grades based on percentile ranks
-        for (int i = 0; i < sortedStudents.length; i++) {
-            Student sortedStudent = sortedStudents[i];
-            double percentileRank = ((double) i) / numStudents;
-            char grade;
-            if (i == sortedStudents.length - 1) {
-                grade = 'F';
-            } else if (percentileRank < .1) {
-                grade = 'A';
-            } else if (percentileRank < .29) {
-                grade = 'B';
-            } else if (percentileRank < .5) {
-                grade = 'C';
-            } else if (percentileRank < .89) {
-                grade = 'D';
-            } else {
-                grade = 'A';
-            }
-            gradeBook.put(sortedStudents[i], grade);
-        }
-        return gradeBook;
+    public Map<Student, Double> getGradeBook() {
+        return null;
     }
+
 }
+
--- src/main/java/com/github/curriculeon/Student.java
+++ src/main/java/com/github/curriculeon/Student.java
@@ -1,81 +1,57 @@
 package com.github.curriculeon;
 
-import java.util.Arrays;
 
 public class Student implements Comparable<Student> {
-    private String firstName;
-    private String lastName;
-    private Double[] examScores;
 
-    public Student(String firstName, String lastName, Double[] examScores) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.examScores = examScores;
+    public Student(String firstName, String lastName, Double[] testScores) {
     }
 
     public Student() {
-        this(null, null, new Double[]{});
+
     }
 
     public String getFirstName() {
-        return firstName;
+        return null;
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
     }
 
     public String getLastName() {
-        return lastName;
+        return null;
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public Double[] getExamScores() {
-        return examScores;
-    }
-
-    public void setExamScores(Double[] examScores) {
-        this.examScores = examScores;
+        return null;
     }
 
     public void addExamScore(double examScore) {
-        Double[] newExamScores = Arrays.copyOf(this.examScores, this.examScores.length + 1);
-        newExamScores[newExamScores.length - 1] = examScore;
-        this.examScores = newExamScores;
     }
+
 
     public void setExamScore(int examNum, double updateScore) {
-        this.examScores[examNum] = updateScore;
     }
 
+
     public Double getAverageExamScore() {
-        if (this.examScores.length == 0) {
-            return 0.0;
-        }
-        double sum = 0.0;
-        for (double score : this.examScores) {
-            sum += score;
-        }
-        return sum / this.examScores.length;
+        return null;
     }
 
     @Override
     public String toString() {
-        return String.format("%s %s -> Exam Scores: %s",
-                this.firstName,
-                this.lastName,
-                Arrays.toString(this.examScores));
+        return null;
     }
 
+    /**
+     * @param studentToCompareAgainst the object to be compared; compare by grade, then by name
+     * @return
+     */
     @Override
-    public int compareTo(Student otherStudent) {
-        int compareByGrade = otherStudent.getAverageExamScore().compareTo(this.getAverageExamScore());
-        if (compareByGrade == 0) {
-            return this.lastName.compareTo(otherStudent.getLastName());
-        }
-        return compareByGrade;
+    public int compareTo(Student studentToCompareAgainst) {
+        return Integer.valueOf(null);
     }
 }
+
