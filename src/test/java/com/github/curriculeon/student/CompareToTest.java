af3d09b2011277395ea94512727c8cb720b4b4e7
git-leon <xleonhunter@gmail.com>
2023-03-19 21:45:07 -0400

    solved

--- src/test/java/com/github/curriculeon/student/CompareToTest.java
+++ src/test/java/com/github/curriculeon/student/CompareToTest.java
@@ -14,16 +14,16 @@ public class CompareToTest {
         Student charlie = new Student("Charlie", "Brown", new Double[]{85.0, 95.0, 100.0});
 
         // Check that Alice is less than Bob (by grade)
-        Assert.assertTrue(alice.compareTo(bob) > 0);
+        Assert.assertTrue(alice.compareTo(bob) < 0);
 
         // Check that Charlie is greater than Alice (by grade)
-        Assert.assertTrue(charlie.compareTo(alice) > 0);
+        Assert.assertTrue(charlie.compareTo(alice) < 0);
 
         // Check that Alice is equal to a new Student object with the same name and grades
         Assert.assertEquals(0, alice.compareTo(new Student(alice.getFirstName(), alice.getLastName(), alice.getExamScores())));
 
         // Check that Bob is less than Alice (by name, since their grades are the same)
-        Assert.assertTrue(bob.compareTo(alice) < 0);
+        Assert.assertTrue(bob.compareTo(alice) > 0);
     }
 
 
@@ -36,10 +36,10 @@ public class CompareToTest {
 
 
         // Check that David is less than Emily (by grade)
-        Assert.assertTrue(david.compareTo(emily) < 0);
+        Assert.assertTrue(david.compareTo(emily) > 0);
 
         // Check that Alice is less than Emily (by grade)
-        Assert.assertTrue(alice.compareTo(emily) < 0);
+        Assert.assertTrue(alice.compareTo(emily) > 0);
     }
 
     @Test
@@ -51,22 +51,22 @@ public class CompareToTest {
         Student frank = new Student("Frank", "Wong", new Double[]{85.0, 90.0, 85.0});
 
         // Check that Alice is less than Bob (by grade)
-        Assert.assertTrue(alice.compareTo(bob) > 0);
+        Assert.assertTrue(alice.compareTo(bob) < 0);
 
         // Check that Charlie is greater than Alice (by grade)
-        Assert.assertTrue(charlie.compareTo(alice) > 0);
+        Assert.assertTrue(charlie.compareTo(alice) < 0);
 
         // Check that Alice is equal to a new Student object with the same name and grades
         Assert.assertEquals(0, alice.compareTo(new Student("Alice", "Smith", new Double[]{80.0, 90.0, 95.0})));
 
         // Check that Frank is greater than Bob (by grade)
-        Assert.assertTrue(frank.compareTo(bob) > 0);
+        Assert.assertTrue(frank.compareTo(bob) < 0);
 
         // Check that Frank is greater than Alice (by grade)
-        Assert.assertTrue(frank.compareTo(alice) < 0);
+        Assert.assertTrue(frank.compareTo(alice) > 0);
 
         // Check that Frank is less than Charlie (by grade)
-        Assert.assertTrue(frank.compareTo(charlie) < 0);
+        Assert.assertTrue(frank.compareTo(charlie) > 0);
     }
 
 }
